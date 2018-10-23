aec639265c27fc07a343719f007cf13de8c86e4e
yunlin <zxc870830@gmail.com>
2018-10-23 20:26:24 +0800

    10.18

---
+++ test_p20/src/test_p20/test_p20.java
@@ -0,0 +1,25 @@
+package test_p20;
+
+public class test_p20 {
+	public static void main(String[]args)
+	{
+		Car car1;
+		car1=new Car();
+		
+		car1.num=1234;
+		car1.gas=-10;
+		
+		car1.show();
+	}
+}
+class Car
+{
+	int num;
+	double gas;
+	
+	void show()
+	{
+		System.out.println("車號是"+this.num);
+		System.out.println("汽油量是"+this.gas);
+	}
+}
---
+++ test_p5/src/test_p5/test_p5.java
@@ -0,0 +1,15 @@
+package test_p5;
+
+public class test_p5 {
+
+	public static void main(String[]args)
+	{
+		Car car1;
+		car1=new Car();
+	}
+	
+}
+class Car
+{
+	
+}
